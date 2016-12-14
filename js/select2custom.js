42486b50092da897d9d19a67db5f8155a8624b1e
Margar <ma@llo.su>
2016-12-14 18:22:52 +0300

    Правки для нового инпута мобиного номера телефона

---
+++ js/select2custom.js
@@ -0,0 +1,9 @@
+$('.phone-country.in-phone_style_one').select2();
+$(".phone-country.in-phone_style_one").select2({
+    minimumResultsForSearch: Infinity
+});
+
+$('.phone-country.in-phone_style_two').select2();
+$(".phone-country.in-phone_style_two").select2({
+    minimumResultsForSearch: Infinity
+});
