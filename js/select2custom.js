b0bcbb29023b1073daa9c8f9a215817f146707f4
Margar <ma@llo.su>
2016-12-16 11:18:27 +0300

    Новый стиль для выпадающего списка select2

--- js/select2custom.js
+++ js/select2custom.js
@@ -1,9 +1,16 @@
-$('.phone-country.in-phone_style_one').select2();
+
 $(".phone-country.in-phone_style_one").select2({
-    minimumResultsForSearch: Infinity
+  minimumResultsForSearch: Infinity,
+  theme: 'style_wide'
 });
 
-$('.phone-country.in-phone_style_two').select2();
+
 $(".phone-country.in-phone_style_two").select2({
-    minimumResultsForSearch: Infinity
+  minimumResultsForSearch: Infinity,
+  theme: 'style_small'
+});
+
+$('.drop_style_main').select2({
+    minimumResultsForSearch: Infinity,
+    theme: 'drop_style_main'
 });
